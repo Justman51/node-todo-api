3ce03de571cffcc7fb15cd2fb500b0f0e7a95cb6
Jeffrey Ololu <jeffreyololu@Jeffreys-iMac.local>
2018-07-22 17:33:22 +0200

    modified the GET/todos/:id route

--- server/server.js
+++ server/server.js
@@ -57,14 +57,17 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res)=> {
    var id = req.params.id
    if(!ObjectID.isValid(id)) {
-     return res.send();
+     return res.status(404).send();
    }
   
    var todo = Todo.findById(id).then((todo) => {
+     if(!todo) {
+      return res.status(404).send();
+     }
      res.send({
        todo
      });
-  }, (e) => {
+  }).catch((e) => {
     res.status(400).send(e);
   });
 
